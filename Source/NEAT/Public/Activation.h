538d77098bb8198236f1f33127786547ad8ca998
Mute <40270424+Ced0@users.noreply.github.com>
2022-10-10 20:36:43 +0200

    Add activation function

--- Source/NEAT/Public/Activation.h
+++ Source/NEAT/Public/Activation.h
@@ -8,3 +8,18 @@
 typedef float (*ActivationFunction) (float value);
 
 inline float sigmoidActivation(float x) { return 1 / (1 + exp(-x)); };
+
+inline float reluActivation(float x) { return (x > 0 ? x : 0); };
+
+inline float linearActivation(float x) { return x; };
+
+inline float thresholdActivation(float x) { return (x >= 0 ? 1 : 0); };
+
+inline float sinActivation(float x) { return sin(x); };
+
+//Hyperbolic tangent
+inline float hyperTanActivation(float x) { return (1 - exp(-x * 2)) / (1 + exp(-x*2)); };
+
+inline float tanhActivation(float x) { return tanh(x); };
+
+inline float sincActivation(float x) { return sin(x)/x; };
