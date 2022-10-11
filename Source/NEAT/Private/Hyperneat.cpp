6e37b2d34af9b098fc5a16ab1f38bda3efcc1e2f
Mute <40270424+Ced0@users.noreply.github.com>
2022-10-11 14:10:47 +0200

    Hyperneat-LEO

--- Source/NEAT/Private/Hyperneat.cpp
+++ Source/NEAT/Private/Hyperneat.cpp
@@ -4,12 +4,23 @@
 #include "Hyperneat.h"
 
 template<unsigned int S>
-Hyperneat<S>::Hyperneat(unsigned int _populationSize, NeatParameters _neatParamters, ActivationFunction _activationFunction, ThresholdFunction _thresholdFunction)
+Hyperneat<S>::Hyperneat(unsigned int _populationSize, NeatParameters _neatParamters, int _cppnInput, int _cppnOutput,
+	ActivationFunction _activationFunction, ThresholdFunction _thresholdFunction, CppnInputFunction _cppnInputFunction, WeightModifierFunction _weightModifierFunction,
+	std::vector<float> _thresholdConstants, std::vector<float> _inputConstants, std::vector<float> _weightConstants)
 {
 	activationFunction = _activationFunction;
 	thresholdFunction = _thresholdFunction;
+	cppnInputFunction = _cppnInputFunction;
+	weightModifierFunction = _weightModifierFunction;
 
-	cppns = CPPN_Neat(_populationSize, S, 1, _neatParamters);
+	thresholdConstants = _thresholdConstants;
+	inputConstants = _inputConstants;
+	weightConstants = _weightConstants;
+
+	cppnInput = _cppnInput;
+	cppnOutput = _cppnOutput;
+
+	cppns = CPPN_Neat(_populationSize, cppnInput, cppnOutput, _neatParamters);
 
 	networks.clear();
 }
@@ -25,4 +36,47 @@ void Hyperneat<S>::clear()
 	inputSubstrate.clear();
 	outputSubstrate.clear();
 	hiddenSubstrate.clear();
+}
+
+template<unsigned int S>
+void Hyperneat<S>::generateNetworks()
+{
+
+}
+
+std::deque<float> basicCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2)
+{
+	std::deque<float> input;
+	input.insert(input.end(), p1.begin(), p1.end());
+	input.insert(input.end(), p2.begin(), p2.end());
+	return input;
+}
+
+std::deque<float> sqrDistCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2)
+{
+	std::deque<float> input;
+	input.insert(input.end(), p1.begin(), p1.end());
+	input.insert(input.end(), p2.begin(), p2.end());
+	input.push_back(0);
+
+	for (int i = 0; i < p1.size(); i++)
+	{
+		input[input.size() - 1] += (p2[i] - p1[i]) * (p2[i] - p1[i]);
+	}
+
+	return input;
+}
+
+std::deque<float> deltaDistCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2)
+{
+	std::deque<float> input;
+	input.insert(input.end(), p1.begin(), p1.end());
+	input.insert(input.end(), p2.begin(), p2.end());
+
+	for (int i = 0; i < p1.size(); i++)
+	{
+		input.push_back((p2[i] - p1[i]));
+	}
+
+	return input;
 }
--- Source/NEAT/Public/Hyperneat.h
+++ Source/NEAT/Public/Hyperneat.h
@@ -4,18 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "CPPN_Neat.h"
+#include <deque>
 
-typedef bool (*ThresholdFunction) (std::vector<float> values);
+typedef bool (*ThresholdFunction) (std::vector<float> constants, std::vector<float> values, std::vector<float> p1, std::vector<float> p2);
+
+//Returns the values to pass in input of the CPPN
+typedef std::deque<float> (*CppnInputFunction) (std::vector<float> constants, std::vector<float> p1, std::vector<float> p2);
+
+//Returns the weight to apply to the connection
+typedef float (*WeightModifierFunction) (std::vector<float> constants, float weight, std::vector<float> p1, std::vector<float> p2);
 
 /**
- * 
  * This implementation supposes that we use the same activation function as for all the substrate
+ * S: geometric dimension
  */
 template<unsigned int S>
 class NEAT_API Hyperneat
 {
 public:
-	Hyperneat(unsigned int _populationSize, NeatParameters _neatParamters, ActivationFunction _activationFunction, ThresholdFunction _thresholdFunction);
+	Hyperneat(unsigned int _populationSize, NeatParameters _neatParamters, int _cppnInput, int _cppnOutput,
+		ActivationFunction _activationFunction, ThresholdFunction _thresholdFunction, CppnInputFunction _cppnInputFunction, WeightModifierFunction _weightModifierFunction,
+		std::vector<float> _thresholdConstants = std::vector<float>(), std::vector<float> _inputConstants = std::vector<float>(), std::vector<float> _weightConstants = std::vector<float>());
 
 	~Hyperneat();
 
@@ -30,22 +39,45 @@ public:
 protected:
 	std::list<std::array<float, S>> inputSubstrate;
 	std::list<std::array<float, S>> outputSubstrate;
-	std::vector<std::list<std::array<float, S>>> hiddenSubstrates;
+	std::list<std::list<std::array<float, S>>> hiddenSubstrates;
 
 	std::vector<NeuralNetwork> networks;
 
 	ActivationFunction activationFunction;
 	ThresholdFunction thresholdFunction;
+	CppnInputFunction cppnInputFunction;
+	WeightModifierFunction weightModifierFunction;
 
 	CPPN_Neat cppns;
+
+	int cppnInput, cppnOutput;
+	std::vector<float> thresholdConstants, inputConstants, weightConstants;
 };
 
+//CPPN Input Functions
+std::deque<float> basicCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2);
+std::deque<float> sqrDistCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2);
+std::deque<float> deltaDistCppnInput(std::vector<float> constants, std::vector<float> p1, std::vector<float> p2);
 
 //Threshold functions
-
-//To test if it really works
-template<unsigned int T>
-bool fixedThreshold(std::vector<float> values)
+inline bool fixedThreshold(std::vector<float> constants, std::vector<float> values, std::vector<float> p1, std::vector<float> p2)
 {
-	return (values[0] > T);
+	return (constants[0] < abs(values[0]));
+}
+
+
+inline bool leoThreshold(std::vector<float> constants, std::vector<float> values, std::vector<float> p1, std::vector<float> p2)
+{
+	return (constants[0] < abs(values[1]));
+}
+
+//Weight modifiers functions
+inline float noChangeWeight(std::vector<float> constants, float weight, std::vector<float> p1, std::vector<float> p2)
+{
+	return weight;
+}
+
+inline float proportionnalWeight (std::vector<float> constants, float weight, std::vector<float> p1, std::vector<float> p2)
+{
+	return constants[0] * weight;
 }
